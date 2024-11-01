9c5c90eadab98bb51ad953f25b286d197a39d866
tmongstad <tmongstad@gmail.com>
2024-11-01 09:34:09 +0100

    Created folder setup for mvc

--- app.py
+++ app.py
@@ -59,13 +59,13 @@ def manage_cars():
 
 # Route for getting, updating and deleting specific car-data
 @app.route('/cars/<car_id>', methods = ['GET', 'PUT', 'DELETE'])
-def handle_cars():
+def handle_cars(car_id):
     if request.method == 'GET':
-        return get_car()
+        return get_car(car_id)
     if request.method == 'PUT':
-        return update_car()
+        return update_car(car_id)
     if request.method == 'DELETE':
-        return delete_car()
+        return delete_car(car_id)
 
 # Gets all cars from database
 def get_all_cars():
@@ -107,6 +107,81 @@ def add_car():
         "Car":returned_data
         }), 201
 
+def get_car(car_id):
+    try:
+        car_id = int(car_id)
+    except ValueError:
+        return jsonify({
+            "Status": "Error",
+            "Message": "The 'car_id' field must be an integer."
+        }), 400
+    query = "MATCH (car:Car{car_id:$car_id}) RETURN car"
+    results = run_query(query, params={'car_id':car_id})
+    if results:
+        return jsonify({ # Return this dict in a json-format.
+            'Status': 'Success!',
+            'data': results
+        }), 200  # Status code that tells us that the requested resource was found
+    else:
+        return jsonify({ # Return this dict in a json-format.
+            "Status": "Error",
+            "Message": f"No cars found with car_id: {car_id}"
+        }), 404  # Status code that tells us that it was not found
+
+def update_car(car_id):
+    try:
+        car_id = int(car_id)
+    except ValueError:
+        return jsonify({
+            "Status": "Error",
+            "Message": "The 'car_id' field must be an integer."
+        }), 400
+    data = request.get_json()
+    fields = {}
+    if 'make' in data:
+        fields['make'] = data['make']
+    if 'model' in data:
+        fields['model'] = data['model']
+    if 'location' in data:
+        fields['location'] = data['location']
+    if 'year' in data:
+        try:
+            data['year'] = int(data['year'])
+        except ValueError:
+            return jsonify({
+            "Status": "Error",
+            "Message": "The 'car_id' field must be an integer."
+        }), 400
+        fields['year'] = data['year']
+    if 'status' in data:
+        fields['status'] = data['status']
+    query_variable = ','.join([f'car.{key}=${key}' for key in fields.keys()])
+    query = f"""
+    MATCH (car:Car{{car_id:$car_id}})
+    SET {query_variable}
+    RETURN car
+    """
+    results = run_query(query, {'car_id':car_id, **fields})
+    if results:
+        return jsonify({
+            "Status": "Success",
+            "Message": f"Car with ID {car_id} updated successfully.",
+            "Updated Data": results[0]
+        }), 200
+    else:
+        return jsonify({
+            "Status": "Error",
+            "Message": f"No car found with car_id: {car_id}"
+        }), 404
+    
+def delete_car(car_id):
+    try:
+        car_id = int(car_id)
+    except ValueError:
+        return jsonify('Not an integer')
+    query = "MATCH (car:Car {car_id:$car_id}) DETACH DELETE car RETURN COUNT(car) AS deleted_count"
+    result = run_query(query, {'car_id':car_id})
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
