79591aa020fc7ad78be6f282f4a7f87a5d25da56
tmongstad <tmongstad@gmail.com>
2024-11-13 12:57:32 +0100

    Changes in query_management

--- project/Model/query_management.py
+++ project/Model/query_management.py
@@ -1,9 +1,10 @@
 from neo4j import GraphDatabase
+from 
 
 # DATABASE URI and login-details
 NEO4J_URI='neo4j+s://077d22f6.databases.neo4j.io'
 NEO4J_USER='neo4j'
-NEO4J_PASSWORD='Password'
+NEO4J_PASSWORD=os.getenv('NEO4J_PASSWORD')
 
 # Initiate driver
 driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
